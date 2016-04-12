9f177d7183cbd335702b76f72703199f30cbc9bf
lijianzwm <lijian8931@163.com>
2016-04-12 09:11:53 +0800

    添加计数service

---
+++ Application/Common/Service/CountinService.class.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: lijian
+ * Date: 2016/4/12
+ * Time: 8:49
+ */
+
+namespace Common\Service;
+
+class CountinService{
+
+    public static function addCount($phone,$num){
+
+    }
+
+}
