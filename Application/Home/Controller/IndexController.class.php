dbbb45b9355c1666d8978b27a57f9e48d92725a2
lijianzwm <lijian8931@163.com>
2016-04-16 15:24:00 +0800

    添加主页视频，倒计时共修

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -1,6 +1,8 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
+use Common\Service\DateService;
+
 class IndexController extends Controller {
     public function index(){
         if( session("userid")){
@@ -8,6 +10,31 @@ class IndexController extends Controller {
         }else{
             $this->assign("isLogin", 0);
         }
+
+        $todayDate = DateService::getStrDate();
+        $startTime = $todayDate." ".C("GX_START_TIME");
+
+        $currentTime = DateService::getCurrentTime();
+
+        $videoTimeLength = intval(C("GX_VIDEO_TIME_LENGTH"));//共修视频时长多少秒
+
+        $seconds = intval(DateService::timeDistance($currentTime, $startTime));
+        $readyTime = intval(C("GX_READY_TIME")*60);//分钟*秒数
+
+        $state = "end";
+
+        if( $seconds <= 0 ){//在共修开始之前
+            if ($seconds < 0 && $seconds > -$readyTime) {//如果在准备时间内
+                $this->assign("countdownSeconds", -$seconds);
+                $state = "ready";
+            }
+        }else{
+            if( $seconds <= $videoTimeLength ){//如果在共修时
+                $this->assign("begTime", $seconds);
+                $state = "started";
+            }
+        }
+        $this->assign("state", $state);
         $this->display();
     }
 }
