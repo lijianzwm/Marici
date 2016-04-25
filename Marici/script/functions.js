dae221056c8331bdc946e3709b94a23907de7e2d
lijianzwm <lijian8931@163.com>
2016-04-25 12:19:30 +0800

    添加找回密码，成功提示改成toast

--- Marici/script/functions.js
+++ Marici/script/functions.js
@@ -18,3 +18,53 @@ function setUserInfo(user){
         value: user
     });
 }
+
+function autoFillPhone(domPhone){
+    api.getPhoneNumber(
+        function(ret, err){
+            if(ret){
+                phone= ret.value;
+                if( phone[0] == '+'){
+                    phone = phone.substr(3);
+                }
+                domPhone.val(phone);
+            }
+        }
+    );
+}
+
+function sendCode(code, domSendCodeButton,jsSendMessageWait){
+    api.ajax({
+        url : apiHost+'/Api/Message/sendVerifyCode',
+        method : 'post',
+        dataType:'json',
+        data : {
+            values : {
+                apikey : apiKey,
+                phone : phone,
+                code : code
+            }
+        }
+    }, function(ret, err){
+        if( ret ){
+            //TODO 发送成功！
+        }else{
+            alert(JSON.stringify(err))
+        }
+    });
+    time(domSendCodeButton,jsSendMessageWait);
+}
+
+function time(obj,jsSendMessageWait){
+    if( jsSendMessageWait == 0 ){
+        obj.html("发送");
+        isEnableClick = true;
+    }else{
+        isEnableClick = false;
+        jsSendMessageWait -= 1;
+        obj.html(jsSendMessageWait);
+        setTimeout(function(){
+            time(obj,jsSendMessageWait-1);
+        },1000);
+    }
+}
